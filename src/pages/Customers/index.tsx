0261cc9cffd8a6711a4bca5bf754a9197d8b1b5e
Uedson Reis <uedsonreis@MacReis.local>
2021-03-09 14:09:51 -0300

    WIP

--- src/pages/Customers/index.tsx
+++ src/pages/Customers/index.tsx
@@ -2,27 +2,30 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { useNavigation } from '@react-navigation/native';
 import { Button, FlatList, Text, View } from 'react-native';
+import firestore from "@react-native-firebase/firestore";
 import auth from "@react-native-firebase/auth";
 
+import { Customer } from './types';
 import styles from './styles';
 
 export default function Customers() {
 
     const navigation = useNavigation();
 
+    const [logged, setLogged] = React.useState(auth().currentUser);
+    const [customers, setCustomers] = React.useState<Customer[]>([]);
+
     React.useLayoutEffect(() => {
-        const logged = auth().currentUser;
         if (!logged) handleSignOut();
 
         navigation.setOptions({
             headerRight: () => <Button title="Sign Out" onPress={handleSignOut} />
         });
-    }, [navigation, handleSignOut, auth]);
+    }, [navigation, handleSignOut, logged]);
 
-    const [customers, setCustomers] = React.useState([
-        { name: 'Maria', email: 'maria@any.com'  },
-        { name: 'Heitor', email: 'heitor@any.com' },
-    ]);
+    firestore().collection('customers').where('userId', '==', logged?.uid).get().then(response => {
+        setCustomers(response.docs.map(doc => doc.data() as any));
+    });
 
     function handleSignOut() {
         navigation.navigate('login');
@@ -34,7 +37,7 @@ export default function Customers() {
 
             <FlatList
                 data={customers}
-                keyExtractor={user => user.email}
+                keyExtractor={item => item.email}
                 renderItem={({ item }) => (
                     <View style={styles.row}>
                         <Text style={styles.text}>{item.name} - {item.email}</Text>
